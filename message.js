b523e2b31129c170eb8ff1adaaa6a1106054135a
Roman Yatsynych <rom.yatsynych@gmail.com>
2020-07-09 17:11:14 +0300

    changed website

---
+++ message.js
@@ -0,0 +1,40 @@
+let msgButton = document.getElementById("msgButton");
+msgButton.onclick = (e) => {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let message = document.getElementById("message").value;
+    fetch("http://localhost:3000/userdata", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({name, surname, message})
+    })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+let sbcButton = document.getElementById("sbcButton");
+sbcButton.onclick = (e) => {
+    let sbcMail = document.getElementById("sbcMail").value;
+    fetch("http://localhost:3000/usermail", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({sbcMail})
+    })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+
---
+++ message/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const http = require('http').createServer(app);
+const mailer = require('nodemailer');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.use(cors());
+app.post("/usermail", async (request, response) => {
+    const {sbcMail} = request.body;
+    connection.query( `INSERT INTO subscribers(email) VALUES(?)`, [sbcMail]);
+    const transport = mailer.createTransport({
+        service:"Gmail",
+        auth:{
+            user:"rom.yatsynych@gmail.com",
+            pass:"mdgnsfddfctywmia"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    await transport.sendMail({
+        from: "rom.yatsynych@gmail.com",
+        to:sbcMail,
+        subject:"Subscribe",
+        html:"<div><h1>Thank you for subscribe</h1></div>"
+    });
+    response.status(200).send({message: "success"})
+});
+
+http.listen(3000, () => {
+    console.log('3000');
+});
